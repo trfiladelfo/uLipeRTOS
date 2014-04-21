f0e0a878bc7dd35033aed60eaddfef702c9a048f
uLipe <felipe.projetos@terra.com.br>
2014-04-20 21:55:27 -0300

    No significant changes

---
+++ Release/src/subdir.mk
@@ -0,0 +1,40 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/\ ButtonTask.c \
+../src/LedTask.c \
+../src/main.c \
+../src/tasks.c 
+
+OBJS += \
+./src/\ ButtonTask.o \
+./src/LedTask.o \
+./src/main.o \
+./src/tasks.o 
+
+C_DEPS += \
+./src/\ ButtonTask.d \
+./src/LedTask.d \
+./src/main.d \
+./src/tasks.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/\ ButtonTask.o: ../src/\ ButtonTask.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=soft -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -Wall  -g -DSTM32F40_41xxx -DUSE_STDPERIPH_DRIVER -DHSE_VALUE=8000000 -I"../libs/misc/include" -I"../include" -I"../libs/CMSIS/include" -I"../libs/StdPeriph/include" -std=gnu11 -MMD -MP -MF"src/ ButtonTask.d" -MT"src/\ ButtonTask.d" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=soft -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -Wall  -g -DSTM32F40_41xxx -DUSE_STDPERIPH_DRIVER -DHSE_VALUE=8000000 -I"../libs/misc/include" -I"../include" -I"../libs/CMSIS/include" -I"../libs/StdPeriph/include" -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
